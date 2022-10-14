17d0ff82fdc779888fdb20150cbb56ac858a551b
Rick Robinson <Rick.Robinson@ThalesGroup.com>
2022-10-14 16:46:29 -0400

    beginning work to duplicate keys
    initial work on copying keys from src to dst.

--- kc.py
+++ kc.py
@@ -14,21 +14,35 @@ from kmip.pie import client
 from kmip import enums
 from kmip.core.factories import attributes
 
-# Specify KMIP Server and attributes
-c = client.ProxyKmipClient(
-	hostname='192.168.1.184',
-	port='5696',
-	cert='kmipclient.crt',
-	key='kmipclient.key',
-	ca='TrustedCAs.pem',
-#	cert_reqs=ssl.CERT_OPTIONAL, # no longer supported although you may see this in the default pykmip.conf file, if used.
-	ssl_version="PROTOCOL_TLSv1_2",
-	username='kmip_user',
-	password='Guardium123!',
-    config='client',
-    config_file='pykmip.conf'	
+# Specify key source KMIP Server and attributes
+keySource =	client.ProxyKmipClient(
+			hostname='192.168.1.184',
+			port='5696',
+			cert='kmipclient.crt',
+			key='kmipclient.key',
+			ca='TrustedCAs.pem',
+#			cert_reqs=ssl.CERT_OPTIONAL, # no longer supported although you may see this in the default pykmip.conf file, if used.
+			ssl_version="PROTOCOL_TLSv1_2",
+			username='kmip_user',
+			password='Guardium123!',
+    		config='client',
+    		config_file='pykmip.conf'	
 )
 
+# Specify key destination KMIP Server and attributes - in this case it would be the same
+keyDest =	client.ProxyKmipClient(
+			hostname='192.168.1.184',
+			port='5696',
+			cert='kmipclient.crt',
+			key='kmipclient.key',
+			ca='TrustedCAs.pem',
+#			cert_reqs=ssl.CERT_OPTIONAL, # no longer supported although you may see this in the default pykmip.conf file, if used.
+			ssl_version="PROTOCOL_TLSv1_2",
+			username='kmip_user',
+			password='Guardium123!',
+    		config='client',
+    		config_file='pykmip.conf'	
+)
 # Alternative method for specifying KMIP Server.  Uses pykmip.conf file from default location.
 #c = client.ProxyKmipClient()
 
@@ -39,11 +53,13 @@ keyAttribs = f.create_attribute(
 				enums.ObjectType.SYMMETRIC_KEY
 			)
 
+name_index = 0	# To be confirmed below
+
 # Code for locating (READING) keys and associated attributes for user from KMIP server. It limits its search to those
 # attributes defined by the create_attributes object.
 # This call initiates connection with KMIP server
-with c:
-	listOfKeys = c.locate(attributes=[keyAttribs])	
+with keySource:
+	listOfKeys = keySource.locate(attributes=[keyAttribs])	
 
 # The first 'tuple object is just a LIST of key IDs.  However, the second object is a nested tubple of THREE key-valuye pairs 
 # consisting of attribute_name, attribute_index, attribute_value, and attribute_value.  You can print(a) below to see a complete 
@@ -52,13 +68,15 @@ with c:
 	print("\nNumber of Keys: ", len(listOfKeys), "\n")
 	
 	for keyID in listOfKeys:
-		keyValue = c.get(keyID)
+		keyValue = keySource.get(keyID)
 		convert_string_to_int = int(str(keyValue)[2:-1], base=16)
 		convert_hex = hex(convert_string_to_int)
 		print("\nkeyID: ", keyID, "\nKEY Value (hex): ", convert_hex )
-		attriby = c.get_attributes(keyID)
+		attriby = keySource.get_attributes(keyID)
 		idx = 0
 		for a in attriby[1]:
 			print("Idx: ", idx, a.attribute_name, ": ", a.attribute_value)
+			if(str(a.attribute_name) == 'Name'):
+				name_index = idx	#finds index of Name and remembers it for later use.
 			idx = idx + 1
-	
+
