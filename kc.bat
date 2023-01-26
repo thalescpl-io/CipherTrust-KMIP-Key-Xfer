6a59a77b4ef52361185100cf445d14038edbe112
Rick Robinson <Rick.Robinson@ThalesGroup.com>
2023-01-26 10:26:45 -0500

    new CAs and Kyndryl demo
    Added capability to import numerous CAs into file that is to be trusted (TrusteCAs.pem).  In addition, modified batch file to support Kyndryl demo by using new CM instancesl

--- kc.bat
+++ kc.bat
@@ -1,5 +1,6 @@
 REM
 REM kc.py KMIP Key Transfer batch file with values
 REM
-py kc.py -srcHost 192.168.1.184 -srcUser kmip_alice -srcPass Thales123! -dstHost 192.168.1.180 -dstUser kmip_charlie -dstPass Thales345! -clientCert kmip_client.crt -clientKey kmip_client.key -trustedCAs TrustedCAs.pem
+copy /b T_CA*.pem TrustedCAs.pem
+py kc.py -srcHost cm-spock.test256.io -srcUser User-KMIP-Bones -srcPass Photon-1 -dstHost cm-kirk.test256.io -dstUser User-KMIP-Sulu -dstPass Photon-2 -clientCert kmip_client.crt -clientKey kmip_client.key -trustedCAs TrustedCAs.pem
 
