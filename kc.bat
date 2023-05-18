d6c0828c05b8fead9e4bd752f321f3f2e8556549
Rick Robinson <Rick.Robinson@ThalesGroup.com>
2023-05-18 16:06:29 -0400

    changed usernames
    Changes username and password in the batch file.

--- kc.bat
+++ kc.bat
@@ -2,5 +2,5 @@ REM
 REM kc.py KMIP Key Transfer batch file with values
 REM
 copy /b T_CA*.pem TrustedCAs.pem
-py kc.py -srcHost cm-spock.test256.io -srcUser User-KMIP-Bones -srcPass Photon-1 -dstHost cm-kirk.test256.io -dstUser User-KMIP-Sulu -dstPass Photon-2 -clientCert kmip_client.crt -clientKey kmip_client.key -trustedCAs TrustedCAs.pem
+py kc.py -srcHost gde-sun.test256.io -srcUser kmip_alice -srcPass Thales123! -dstHost cm-spock.test256.io -dstUser kmip_bob -dstPass Thales234! -clientCert kmip_client.crt -clientKey kmip_client.key -trustedCAs TrustedCAs.pem
 
